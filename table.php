165fb30fb64811ef663c9a245cdf658595f745e1
Евгений Плотников <bad4iz@gmail.com>
2017-02-03 14:27:48 +0400

    4.1

--- table.php
+++ table.php
@@ -1,91 +1,92 @@
 <?
-function drawTable($cols = 10, $rows = 10, $color="red"){
-    for ($i = 1; $i < $cols; $i++){
+function drawTable($cols = 10, $rows = 10, $color = "red")
+{
+    for ($i = 1; $i < $cols; $i++) {
         echo "<tr>";
-        for ($j = 1; $j < $rows; $j++){
-            if ($j == 1) {
+        for ($j = 1; $j < $rows; $j++) {
+            if (($j == 1) and ($i == 1)) {
                 $style = "style='background-color:$color' ;";
-            }
-            else {
+            } else {
                 $style = "style='background-color:none' ;";
             }
-            echo "<td $style>".($i * $j)."</td>";
+            echo "<td $style>" . ($i * $j) . "</td>";
         }
         echo "</tr>";
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-  <title>Таблица умножения</title>
-  <meta charset="utf-8" />
-  <link rel="stylesheet" href="style.css" />
+    <title>Таблица умножения</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 
 <body>
 
-  <div id="header">
+<div id="header">
     <!-- Верхняя часть страницы -->
-    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo"/>
     <span class="slogan">приходите к нам учиться</span>
     <!-- Верхняя часть страницы -->
-  </div>
+</div>
 
-  <div id="content">
+<div id="content">
     <!-- Заголовок -->
     <h1>Таблица умножения</h1>
     <!-- Заголовок -->
     <!-- Область основного контента -->
     <form action=''>
-      <label>Количество колонок: </label>
-      <br />
-      <input name='cols' type='text' value="" />
-      <br />
-      <label>Количество строк: </label>
-      <br />
-      <input name='rows' type='text' value="" />
-      <br />
-      <label>Цвет: </label>
-      <br />
-      <input name='color' type='text' value="" />
-      <br />
-      <br />
-      <input type='submit' value='Создать' />
+        <label>Количество колонок: </label>
+        <br/>
+        <input name='cols' type='text' value=""/>
+        <br/>
+        <label>Количество строк: </label>
+        <br/>
+        <input name='rows' type='text' value=""/>
+        <br/>
+        <label>Цвет: </label>
+        <br/>
+        <input name='color' type='text' value=""/>
+        <br/>
+        <br/>
+        <input type='submit' value='Создать'/>
     </form>
     <!-- Таблица -->
     <table border='1' width="200">
-    <?
-    drawTable();
-    ?>
+        <?
+        drawTable();
+        ?>
 
     </table>
     <!-- Таблица -->
     <!-- Область основного контента -->
-  </div>
-  <div id="nav">
+</div>
+<div id="nav">
     <h2>Навигация по сайту</h2>
     <!-- Меню -->
     <ul>
-      <li><a href='index.php'>Домой</a>
-      </li>
-      <li><a href='about.php'>О нас</a>
-      </li>
-      <li><a href='contact.php'>Контакты</a>
-      </li>
-      <li><a href='table.php'>Таблица умножения</a>
-      </li>
-      <li><a href='calc.php'>Калькулятор</a>
-      </li>
+        <li><a href='index.php'>Домой</a>
+        </li>
+        <li><a href='about.php'>О нас</a>
+        </li>
+        <li><a href='contact.php'>Контакты</a>
+        </li>
+        <li><a href='table.php'>Таблица умножения</a>
+        </li>
+        <li><a href='calc.php'>Калькулятор</a>
+        </li>
     </ul>
     <!-- Меню -->
-  </div>
-  <div id="footer">
+</div>
+<div id="footer">
     <!-- Нижняя часть страницы -->
     &copy; Супер Мега Веб-мастер, 2000 &ndash; 2015
     <!-- Нижняя часть страницы -->
-  </div>
+</div>
 </body>
 
 </html>
