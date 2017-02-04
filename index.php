20a300eba8137e85f75c1a9f6b0d994565bf8a9f
bad4iz <bad4iz@gmail.com>
2017-02-04 17:36:42 +0400

    5

--- index.php
+++ index.php
@@ -1,33 +1,4 @@
-<?php
-// Установка локали и выбор значений даты
-setlocale(LC_ALL, "russian");
-$hour = (int)strftime('%H');
-$day = strftime('%d');
-$mon = iconv("windows-1251", "UTF-8", strftime('%B'));;
-$year = strftime('%Y');
-$welkome = '';
 
-if ($hour < 7) {
-    $welkome = 'Доброй ночи';
-} elseif ($hour < 12) {
-    $welkome = 'Доброе утро';
-} elseif ($hour < 18) {
-    $welkome = 'Добрый день1';
-} elseif ($hour < 23) {
-    $welkome = 'Добрый вечер';
-} else {
-    $welkome = 'Доброй ночи';
-}
-
-$leftMenu = [['link' => 'Домой', 'href' => 'index.php'],
-    ['link' => 'О нас', 'href' => 'about.php'],
-    ['link' => 'Контакты', 'href' => 'contact.php'],
-    ['link' => 'Таблица умножения', 'href' => 'table.php'],
-    ['link' => 'Калькулятор', 'href' => 'calc.php']
-];
-
-
-?>
 
 
 <!DOCTYPE html>
