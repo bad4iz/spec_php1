69db807d0cded6638400e2fd45967a772e7692e1
bad4iz <bad4iz@gmail.com>
2017-02-04 17:31:07 +0400

    delete drawTable

--- table.php
+++ table.php
@@ -1,23 +1,5 @@
 <?
-function drawTable($cols = 10, $rows = 10, $color = "red")
-{
-    $cols++;
-    $rows++;
-    for ($i = 1; $i < $cols; $i++) {
-        echo "<tr>";
-        for ($j = 1; $j < $rows; $j++) {
-            if ($j == 1)  {
-                echo "<th style='background-color:$color';>" . ($i * $j) . "</th>";
-            } elseif ($i == 1) {
-                echo "<th style='background-color:$color';>" . ($i * $j) . "</th>";
-            } else{
 
-            echo "<td>" . ($i * $j) . "</td>";
-            }
-        }
-        echo "</tr>";
-    }
-}
 
 ?>
 <!DOCTYPE html>
