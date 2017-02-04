4e93bfa1bed2f6454a567969e36898cb3e1f7060
bad4iz <bad4iz@gmail.com>
2017-02-04 17:30:30 +0400

    5

--- inc/lib.inc.php
+++ inc/lib.inc.php
@@ -1,1 +1,13 @@
 <?
+function drawMenu($menu, $vertical = true)
+{
+    echo "<ul>";
+    foreach ($menu as $value) {
+        echo "<li";
+        if (!$vertical) {
+            echo 'style="display: inline-block;"';
+        }
+        echo "><a href='{$value['href']}'>{$value['link']}</a></li>";
+    }
+    echo "</ul>";
+}
