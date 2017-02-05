6b525fb09bf5d7caf381721d7d35235e1fdedafb
bad4iz <bad4iz@gmail.com>
2017-02-05 08:13:51 +0400

    7

--- calc.php
+++ calc.php
@@ -6,29 +6,40 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $operator = trim(strip_tags($_POST['operator']));
 }
 $operator = preg_match('/[-+*\/]/',$operator) ? $operator : false;
-function mySun($num1, $num2,$operator){
-
+function mySum($num1, $num2,$operator){
+    switch ($operator){
+        case '+':
+            return $num1 + $num2;
+        case '-':
+            return $num1 - $num2;
+        case '/':
+            return $num1 / $num2;
+        case '*':
+            return $num1 * $num2;
+    }
 }
 
+$result =   echo "$num1 $operator $num2 = " . mySum($num1,  $num2, $operator)
 
 ?>
-   <?=$operator?>
+
     <!-- Область основного контента -->
-    <form action='<?= $_SERVER['REQUEST_URI']?>' method="post">
-      <label>Число 1:</label>
-      <br />
-      <input name='num1' type='text' />
-      <br />
-      <label>Оператор: </label>
-      <br />
-      <input name='operator' type='text' />
-      <br />
-      <label>Число 2: </label>
-      <br />
-      <input name='num2' type='text' />
-      <br />
-      <br />
-      <input type='submit' value='Считать'>
+    <form action='<?= $_SERVER[' REQUEST_URI ']?>' method="post">
+        <label>Число 1:</label>
+        <br />
+        <input name='num1' type='text' />
+        <br />
+        <label>Оператор: </label>
+        <br />
+        <input name='operator' type='text' />
+        <br />
+        <label>Число 2: </label>
+        <br />
+        <input name='num2' type='text' />
+        <br />
+        <br />
+        <input type='submit' value='Считать'>
     </form>
     <!-- Область основного контента -->
-
+    <h1>Итого</h1>
+    <?=$result:$result?>
